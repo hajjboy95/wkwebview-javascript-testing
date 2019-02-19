c67577818dc43b470230c78190cb991ebecb58af
Ismail El-Habbash <gizmo-boy95@hotmail.com>
2019-02-19 14:28:28 +0800

    Initial Commit

---
+++ webview-bridge-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview-bridge-test
+//
+//  Created by Ismail El-habbash on 2019/2/19.
+//  Copyright © 2019 Ismail El-habbash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
